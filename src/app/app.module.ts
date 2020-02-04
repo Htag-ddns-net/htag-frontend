a8ef29d636ef1f907617aa922f4f43ba3cc1c8bb
Hentai Sex <fapingalot@gmail.com>
2020-02-04 19:41:28 +0100

    Applied best practices for folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    // Angular Imports
+    BrowserModule,
+    AppRoutingModule,
+
+    // Core Module
+    CoreModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    // Angular Imports
+    CommonModule,
+    HttpClientModule,
+  ],
+})
+export class SharedModule {}
